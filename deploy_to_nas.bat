e9fbc13fb2017a141c62aad7bf8cdc386f9c9427
lourusso <lou@spreesolarsystems.com>
2026-09-19 15:09:42 -1000

    Fix PATH variable escaping in deploy_to_nas.bat

--- deploy_to_nas.bat
+++ deploy_to_nas.bat
@@ -13,7 +13,7 @@ echo Connecting via SSH to deploy container using Git on the NAS...
 echo Please enter your Synology password when prompted.
 echo.
 
-ssh -t lou@192.168.42.184 "sudo sh -c 'export PATH=\$PATH:/var/packages/Git/target/bin:/usr/local/bin; TARGET_DIR=\"/volume1/docker/laughing-pig-solar\"; if [ ! -d \"/volume1/docker\" ]; then for v in /volume*/docker; do if [ -d \"\$v\" ]; then TARGET_DIR=\"\$v/laughing-pig-solar\"; break; fi; done; fi; mkdir -p \"\$TARGET_DIR\" && cd \"\$TARGET_DIR\" && if [ -d .git ]; then git pull; else git clone https://github.com/lourusso/laughing-pig-solar-monitor.git .; fi && (docker compose up -d --build || docker-compose up -d --build)'"
+ssh -t lou@192.168.42.184 "sudo env PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/var/packages/Git/target/bin sh -c 'mkdir -p /volume1/docker/laughing-pig-solar && cd /volume1/docker/laughing-pig-solar && if [ -d .git ]; then git pull; else git clone https://github.com/lourusso/laughing-pig-solar-monitor.git .; fi && (docker compose up -d --build || docker-compose up -d --build)'"
 
 echo.
 echo ====================================================================
